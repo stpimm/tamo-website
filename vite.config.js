e1c3a86480082420e13d4f68c470661b2047e9c9
Immanuel Sitepu <66082265+stpimm@users.noreply.github.com>
2025-08-10 17:25:19 +0700

    vite.config.js edited

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwind()],
-  // IMPORTANT: use your repo name (project page)
-  base: '/tamo-wesbite/',
-  // Build straight into /docs so Pages can serve it from main/docs
+  base: '/tamo-website/',       // 👈 must match your repo name exactly
   build: { outDir: 'docs', emptyOutDir: true }
 })
